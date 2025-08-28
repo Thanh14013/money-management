f243e9b8670d4ff4bc58e55606971bfb9d7f2665
Nguyen Vu Thanh <thanh14704@gmail.com>
2025-08-28 11:03:02 +0700

    profile register logic

---
+++ src/main/java/com/thanh1407/moneymanagement/controller/ProfileController.java
@@ -0,0 +1,25 @@
+package com.thanh1407.moneymanagement.controller;
+
+import com.thanh1407.moneymanagement.dto.ProfileDTO;
+import com.thanh1407.moneymanagement.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO){
+        ProfileDTO registeredProfile = profileService.registerProfile(profileDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+    }
+
+}
